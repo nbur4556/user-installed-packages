cc10d319d6a2f01a3030ccdbcf9f77ba6d12e928
Nick Burt <nbur4556@gmail.com>
2023-11-08 07:32:17 -0600

    read file at a given file path

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import readline
 import typer
 
 app = typer.Typer()
 
 @app.command()
-def hello():
-    history_length = readline.get_history_length()
-    if history_length <= 0:
-        print("No history found")
+def read_file(filepath: str):
+    history_file = open(filepath)
+    print(history_file.read())
 
-    for i in range(readline.get_history_length()):
-        print(readline.get_history_item(i+1))
 
 if __name__ == "__main__":
     app()
