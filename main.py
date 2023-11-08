be0060b4d8a3bedde0b935a21f24ba47c4bf40d9
Nick Burt <nbur4556@gmail.com>
2023-11-08 07:41:53 -0600

    filter all history with apt install included

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ app = typer.Typer()
 
 @app.command()
 def read_file(filepath: str):
-    history_file = open(filepath)
-    print(history_file.read())
+    history_file = open(filepath, "r")
+    history = []
+
+    for line in history_file:
+        if line.__contains__("apt install"):
+            print(line)
+            history.append(line)
+
     history_file.close()
 
 
