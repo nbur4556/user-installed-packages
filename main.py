53cc34c1b3741aae632a03dc6bf126b68e37199c
Nick Burt <nbur4556@gmail.com>
2023-11-08 07:14:32 -0600

    read through lines of default history location and return message if not found

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import readline
 import typer
 
 app = typer.Typer()
 
 @app.command()
 def hello():
-    print("Hello World")
+    history_length = readline.get_history_length()
+    if history_length <= 0:
+        print("No history found")
+
+    for i in range(readline.get_history_length()):
+        print(readline.get_history_item(i+1))
 
 if __name__ == "__main__":
     app()
